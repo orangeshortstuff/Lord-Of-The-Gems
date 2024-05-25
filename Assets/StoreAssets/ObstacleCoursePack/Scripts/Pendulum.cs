eb134cc68ae70a999525d1e3b78ff31fc0509429
DanielM2000 <114746553+DanielM2000@users.noreply.github.com>
2024-05-25 02:21:38 +0100

    Bug fixed with boulder directions

--- Assets/StoreAssets/ObstacleCoursePack/Scripts/Pendulum.cs
+++ Assets/StoreAssets/ObstacleCoursePack/Scripts/Pendulum.cs
@@ -12,33 +12,24 @@ public class Pendulum : MonoBehaviour
     public MovementDirection movementDirection = MovementDirection.LeftRight; // Default to left-right movement
     private float random = 0;
 
-    // Start is called before the first frame update
     void Awake()
     {
         if (randomStart)
             random = Random.Range(0f, 1f);
     }
 
-    // Update is called once per frame
     void Update()
     {
         float angle = limit * Mathf.Sin(Time.time * speed + random);
-        Vector3 rotationAxis;
 
         switch (movementDirection)
         {
             case MovementDirection.LeftRight:
-                rotationAxis = new Vector3(0, 1, 0); // Rotate around y-axis for left-right movement
+                transform.localRotation = Quaternion.Euler(0, 0, angle); // Swing left-right
                 break;
             case MovementDirection.ForwardBackward:
-                rotationAxis = new Vector3(1, 0, 0); // Rotate around x-axis for forward-backward movement
-                break;
-            default:
-                rotationAxis = new Vector3(0, 1, 0);
+                transform.localRotation = Quaternion.Euler(angle, 0, 0); // Swing forward-backward
                 break;
         }
-
-        Quaternion rotation = Quaternion.Euler(rotationAxis.x * angle, rotationAxis.y * angle, rotationAxis.z * angle);
-        transform.localRotation = rotation;
     }
 }
