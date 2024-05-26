d51be02f4927cfa576c310686d941f97ab5f4a4b
Alexander Walker <22206619@bucks.ac.uk>
2024-05-26 21:34:23 +0100

    ui bits for colo

--- Assets/Scripts/GemWall.cs
+++ Assets/Scripts/GemWall.cs
@@ -6,12 +6,14 @@ public class GemDoor : MonoBehaviour
 {
     public ScoreManager sm;
     public int gemRequirement;
+    public GameObject player;
     PickupMessage message;
     // Start is called before the first frame update
     void Start()
     {
         sm = GameObject.Find("score").GetComponent<ScoreManager>();
         message = GameObject.Find("powerAlert").GetComponent<PickupMessage>();
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
@@ -20,6 +22,9 @@ public class GemDoor : MonoBehaviour
         if(sm.score >= gemRequirement) {
             message.SetMessage("A gem door has been unlocked.", 5);
             gameObject.SetActive(false); // disable the running of all scripts, renderers, colliders, etc.
+        }
+        if (Vector3.Distance(transform.position, player.transform.position) < 10f) {
+            message.SetMessage("Collect all the gems first!", 2);
         }
     }
 }
