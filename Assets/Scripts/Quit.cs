1d26b0c11aceaa58f036a1d1f7c6ad516e0d7744
Alexander Walker <22206619@bucks.ac.uk>
2024-05-29 22:44:22 +0100

    selection works after exiting to menu

--- Assets/Scripts/Quit.cs
+++ Assets/Scripts/Quit.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Quit : MonoBehaviour
 {
+    void Start() {
+        Cursor.lockState = CursorLockMode.None; // free the cursor
+        Cursor.visible = true; // and make it visible
+    }
     public void OnClick()
     {
         Application.Quit();
